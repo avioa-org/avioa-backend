1d450bfcaba21c0b68283df8592aba0bfba91f07
apoyotecnologia02-gif <apoyotecnologia02@gmail.com>
2026-07-16 11:30:36 -0500

    feat: remove Gmail infrastructure service from cron module providers

--- src/jobs/cron.module.ts
+++ src/jobs/cron.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronService } from './cron.service';
-import { GmailInfraService } from 'src/infrastructure/gmail-infra/gmail.infra';
 import { ConfigService } from '@nestjs/config';
 import { EvolutionApiService } from 'src/infrastructure/evolution-api/evolution-api.service';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
@@ -9,13 +8,7 @@ import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [ScheduleModule.forRoot(), HttpModule],
-  providers: [
-    CronService,
-    GmailInfraService,
-    ConfigService,
-    EvolutionApiService,
-    PrismaService,
-  ],
+  providers: [CronService, ConfigService, EvolutionApiService, PrismaService],
   exports: [CronService],
 })
 export class CronModule {}
